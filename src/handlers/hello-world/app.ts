4e917f660b0101ea2cb79bad78843033ef51672c
Yasuyuki Saito <ya_saito@uluru.jp>
2021-08-09 16:47:12 +0900

    utils追加

--- src/handlers/hello-world/app.ts
+++ src/handlers/hello-world/app.ts
@@ -1,21 +1,21 @@
 import type { Context } from 'aws-lambda';
+import { getMessage } from './libs/utils';
 
 exports.lambdaHandler = async (event: unknown, context: Context) => {
-  let response;
   try {
     console.log(`event: ${JSON.stringify(event)}, context: ${JSON.stringify(context)}`);
     // const ret = await axios(url);
-    response = {
+
+    const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world ts',
+        message: `hello world ts - ${getMessage()}`,
         // location: ret.data.trim()
       }),
     };
+    return response;
   } catch (err) {
     console.log(err);
     return err;
   }
-
-  return response;
 };
